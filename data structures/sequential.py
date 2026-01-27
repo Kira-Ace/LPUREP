eec4a2edceab122ba20534e127a29938a15102b3
Kira-Ace <legaspijazztinnkyle@gmail.com>
2026-01-27 14:34:40 +0800

    transferred data structures

---
+++ data structures/sequential.py
@@ -0,0 +1,24 @@
+#simple sequential search
+
+alist = [1, 3, 5, 7, 9, 11, 13, 15, 4]
+if int(input("enter number:")) in alist:
+    print("found!")
+else:
+    print("not here")
+    
+
+#return index sequential search (where it is)
+def linearSearch(arr, targetVal):
+    for i in range(len(arr)):
+        if arr[i] == targetVal:
+            return i
+    return -1
+    
+anotherlist = [4, 8, 12, 16, 20, 24, 28, 32]
+x = int(input("enter number:"))
+result = linearSearch(anotherlist, x)
+
+if result != -1:
+    print("Found at index", result)
+else:
+    print("not found!")
